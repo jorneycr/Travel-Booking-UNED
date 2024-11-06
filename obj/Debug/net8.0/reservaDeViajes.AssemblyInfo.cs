88d7fcebb6eac92cc4a4e73bac576bd749575670
jorneycr <jorneycr@hotmail.com>
2024-11-05 21:06:56 -0600

    modify tag title

--- obj/Debug/net8.0/reservaDeViajes.AssemblyInfo.cs
+++ obj/Debug/net8.0/reservaDeViajes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reservaDeViajes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b29de81861dcfc01b9a22c73a802e9c5f2666dda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c794edb96f062d4b0e992f4cd3f1ba063972d30")]
 [assembly: System.Reflection.AssemblyProductAttribute("reservaDeViajes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reservaDeViajes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
